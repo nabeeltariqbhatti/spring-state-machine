1d3dd951d13fe482ed338508039ccc7bda15065b
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2021-06-28 11:07:35 +0500

    Enums Added

---
+++ src/main/java/learner/springboot/springsm/ssm/domain/PayementEvent.java
@@ -0,0 +1,6 @@
+package learner.springboot.springsm.ssm.domain;
+
+public enum PayementEvent {
+    PRE_AUTHORIZE, PRE_AUTH_APPROVED,
+    PRE_AUTH_DECLINED, AUTHORIZE,AUTH_APPROVED,AUTH_DECLINED
+}
---
+++ src/main/java/learner/springboot/springsm/ssm/domain/PaymentState.java
@@ -0,0 +1,6 @@
+package learner.springboot.springsm.ssm.domain;
+
+public enum PaymentState {
+
+    NEW,AUTH,PRE_AUTH,PRE_AUTH_ERROR,AUTH_ERROR
+}
